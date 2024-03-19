496e8bb0eff2a7001ee2222856960086ea409638
erna novia <ernanoviasafitri4@gmail.com>
2024-03-19 12:26:33 +0700

    book appoitment success select Tokyo CURA Healthcare Center

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -34,7 +34,7 @@ describe('login testcase', () => {
     cy.get('.text-danger').should('contain.text', "Login failed!")
   })
 
-  it.only('multiple login failed', () => {
+  it('multiple login failed', () => {
     cy.fixture('user.json').then((user) => {
       user.failed_login.forEach((datauser) => {
         cy.login(datauser.username, datauser.password)
@@ -42,4 +42,22 @@ describe('login testcase', () => {
       })
     })
   })
+  it('book appoitment success select Tokyo CURA Healthcare Center', () => {
+    cy.login('John Doe', 'ThisIsNotAPassword')
+    cy.get('#combo_facility').select('Tokyo CURA Healthcare Center')
+    cy.get('#chk_hospotal_readmission').click()
+    cy.contains('Medicaid').click()
+
+    cy.log(daysjs().format('DD/MM/YYYY')) // print date
+    cy.get('#txt_visit_date').type(daysjs().format('DD/MM/YYYY'))
+    // cy.get(':nth-child(5) > .col-sm-offset-3').click()
+    cy.contains('Comment').click()
+
+    cy.get('#txt_comment').type('ini contoh komen untuk bookin appoitment')
+    cy.get('#btn-book-appointment').click()
+    cy.contains('Go to Homepage').click()
+    cy.contains('Make Appointment')
+  })
+  
 })
+
