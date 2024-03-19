e3e9038ae4f050aa33ca29b84a9f9b1d9ab08863
erna novia <ernanoviasafitri4@gmail.com>
2024-03-19 12:20:00 +0700

    add multiple testcase booking success with fixture and custom command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,9 +24,25 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
+import loginPagefrom from "../support/pageObject/loginPage"
+import appoinmentPage from "../support/pageObject/appoinmentPage"
+const userr = require('../fixtures/user.json')
+const daysjs = require('dayjs')
+
 Cypress.Commands.add('login', (username, password) => {
     cy.get('#btn-make-appointment').click()
     cy.get('#txt-username').type(username)
     cy.get('#txt-password').type(password)
     cy.get('#btn-login').click()
+})
+
+Cypress.Commands.add('bookappoinment', (facility, healthcareProgram, comment) => {
+    appoinmentPage.inputFacility(facility)
+    appoinmentPage.inputReadmission()
+    appoinmentPage.inputHealthProgram(healthcareProgram)
+    appoinmentPage.inputVisitDate(daysjs().format('DD/MM/YYYY'))
+    cy.contains('Comment').click()
+    appoinmentPage.inputComment(comment)
+    appoinmentPage.clikBookBtn()
+    appoinmentPage.clickGotohomeBtn()
 })
---
+++ cypress/support/pageObject/appoinmentPage.js
@@ -0,0 +1,48 @@
+class AppoinmentPage {
+    facilityField = '#combo_facility'
+    readmissionCheklist = '#chk_hospotal_readmission'
+    // healthProgramField = 
+    visitDateField = '#txt_visit_date'
+    commentField = '#txt_comment'
+    bookBtn = '#btn-book-appointment'
+    datePicker = '.datepicker'
+
+    inputFacility(facility) {
+        cy.get(this.facilityField).select(facility)
+    }
+
+    inputReadmission(readmissionCheklist) {
+        cy.get(this.readmissionCheklist).click()
+    }
+
+    inputHealthProgram(healthProgram) {
+        cy.contains(healthProgram).click()
+    }
+
+    inputVisitDate(visitdate) {
+        cy.get(this.visitDateField).type(visitdate)
+    }
+
+    inputComment(comment) {
+        cy.get(this.commentField).type(comment)
+    }
+
+    clikBookBtn(bookBtn) {
+        cy.get(this.bookBtn).click()
+    }
+
+    verifyBookError(errorMsg) {
+        cy.contains(errorMsg)
+    }
+
+    clickGotohomeBtn(homeBtn) {
+        cy.contains('Go to Homepage').click()
+    }
+
+    verifyUrlDaasboard(urlDasboard){
+        cy.url().should('include', urlDasboard)
+    }
+
+}
+
+export default new AppoinmentPage()
